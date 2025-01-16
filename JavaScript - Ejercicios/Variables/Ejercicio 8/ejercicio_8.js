2bb886023e75f6af09ecbad77b33dad2ab1a1d2e
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-01-15 20:06:30 -0500

    Ejercicio 8 - JavaScript

---
+++ JavaScript - Ejercicios/Variables/Ejercicio 8/ejercicio_8.js
@@ -0,0 +1,8 @@
+const nombre = "Edison"
+const apellido = "Salinas"
+const edad = 25
+const fechaNacimiento = "05/08/1999"
+const direccion = "Guayaquil"
+  
+// Utilizo interpolación
+console.log(`Mi nombre es ${nombre} ${apellido}, tengo ${edad} años, mi fecha de nacimiento es ${fechaNacimiento}, soy de la ciudad de ${direccion}.`);

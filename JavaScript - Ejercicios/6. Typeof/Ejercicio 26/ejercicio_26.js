ced6a7b3f30f8736d8c0c1d4507049290cef0bd9
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-12 11:53:39 -0500

    add: Ejercicio 26 - Typeof - JS

---
+++ JavaScript - Ejercicios/6. Typeof/Ejercicio 26/ejercicio_26.js
@@ -0,0 +1,20 @@
+const entidadCapacitacion = "Comunidad IT.org";
+const nombreCapacitacion = "React Native";
+const docenteCapacitacion = "Nicolas Isnardi";
+const esOnline = true;
+const cantidadAlumnos = 20;
+const diasCapacitacion = ["lunes","miércoles","viernes"];
+
+console.log("entidadCapacitacion: ", entidadCapacitacion);
+console.log("nombreCapacitacion: ", nombreCapacitacion);
+console.log("docenteCapacitacion: ", docenteCapacitacion);
+console.log("esOnline: ", esOnline);
+console.log("cantidadAlumnos: ", cantidadAlumnos);
+console.log("diasCapacitacion: ", diasCapacitacion);
+
+console.log("entidadCapacitacion: ", typeof(entidadCapacitacion));
+console.log("nombreCapacitacion: ", typeof(nombreCapacitacion));
+console.log("docenteCapacitacion: ", typeof(docenteCapacitacion));
+console.log("esOnline: ", typeof(esOnline));
+console.log("cantidadAlumnos: ", typeof(cantidadAlumnos));
+console.log("diasCapacitacion: ", typeof(diasCapacitacion));

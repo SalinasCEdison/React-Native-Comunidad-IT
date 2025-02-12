5b0e9d229a9265d457e0b693e370500e75248fa0
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-12 10:15:55 -0500

    add: Ejercicio 21 - Numbers - JS

---
+++ JavaScript - Ejercicios/3. Numbers/Ejercicio 21/ejercicio_21.js
@@ -0,0 +1,6 @@
+const edad = 25;
+const telefono = 958634857;
+const calle = "Baquerizo Moreno";
+const altura = 245;
+
+console.log(`Tengo ${edad} años\nMi teléfono es 0${telefono}\nVivo en ${calle} al ${altura}`)

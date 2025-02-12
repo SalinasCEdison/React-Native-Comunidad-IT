ff0560f16aa047dc388d7b4f840c11ca1e666d0b
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-12 12:20:40 -0500

    add: Ejercicio 27 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 27/ejercicio_27.js
@@ -0,0 +1,8 @@
+const numero1 = 23;
+const numero2 = 5;
+
+console.log(`sumo ${numero1} + ${numero2} y el resultado es ${numero1 + numero2}`);
+console.log(`resto ${numero1} - ${numero2} y el resultado es ${numero1 - numero2}`);
+console.log(`multiplico ${numero1} * ${numero2} y el resultado es ${numero1 * numero2}`);
+console.log(`divido ${numero1} / ${numero2} y el resultado es ${numero1 / numero2}`);
+console.log(`el resto de ${numero1} % ${numero2} y el resultado es ${numero1 % numero2}`);

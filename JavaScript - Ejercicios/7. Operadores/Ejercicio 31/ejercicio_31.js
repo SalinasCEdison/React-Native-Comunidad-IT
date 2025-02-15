ece455d72ed0bdc3c862adc604be76dfdbdcd255
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-15 06:59:55 -0500

    add: Ejercicio 31 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 31/ejercicio_31.js
@@ -0,0 +1,7 @@
+const lado1 = 10;
+const lado2 = 20;
+const lado3 = 5;
+const perimetroTriangulo = lado1 + lado2 + lado3;
+
+console.log(`-------- Perímetro de un Triángulo --------`);
+console.log(`Triángulo con lado 1 de ${lado1}, lado 2 de ${lado2} y lado 3 de ${lado3}. Su perímetro es ${perimetroTriangulo}`);

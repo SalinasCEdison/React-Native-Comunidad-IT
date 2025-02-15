f3fcf043ad5a38670e174713f71002b2b38a1411
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-15 06:51:41 -0500

    add: Ejercicio 30 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 30/ejercicio_30.js
@@ -0,0 +1,5 @@
+const longitudLado = 5;
+const areaCuadrado = longitudLado * longitudLado;
+
+console.log("------- Área de un Cuadrado -------");
+console.log(`Cuadrado de longitud ${longitudLado} su área es ${areaCuadrado}`);

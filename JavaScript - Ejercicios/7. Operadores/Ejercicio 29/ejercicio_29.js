0fe2ecec614e54010b789fbf08d0c2dd5eea2aa2
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-15 06:41:17 -0500

    add: Ejercicio 29 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 29/ejercicio_29.js
@@ -0,0 +1,5 @@
+const longitudLado = 10;
+const perimetroCuadrado = longitudLado * 4;
+
+console.log("------- Perímetro de un Cuadrado -------");
+console.log(`Cuadrado de longitud ${longitudLado} su perímetro es ${perimetroCuadrado}`);

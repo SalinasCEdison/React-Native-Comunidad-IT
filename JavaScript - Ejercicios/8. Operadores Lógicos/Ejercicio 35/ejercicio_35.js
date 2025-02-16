226da7aadf65da2419bad84ea9fe82c0b32f9916
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-16 16:16:02 -0500

    add: Ejercicio 35 - Operadores Lógicos - JS

---
+++ JavaScript - Ejercicios/8. Operadores Lógicos/Ejercicio 35/ejercicio_35.js
@@ -0,0 +1,8 @@
+const numeroDeCliente = 143245;
+const numeroDeClienteIngresado = '143245';
+const comparacionValor = numeroDeCliente == numeroDeClienteIngresado;
+const comparacionTipo = numeroDeCliente === numeroDeClienteIngresado;
+
+console.log(`El número ingresado por el cliente es correcto: ${comparacionValor}`);
+
+console.log(`El cliente ingresó un número de cliente válido: ${comparacionTipo}`);

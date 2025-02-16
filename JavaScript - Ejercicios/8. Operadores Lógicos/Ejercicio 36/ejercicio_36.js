e77ec44eaa0bd0dc1b512fa3c22e4f5e2f675971
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-16 16:21:07 -0500

    add: Ejercicio 36 - Operadores Lógicos - JS

---
+++ JavaScript - Ejercicios/8. Operadores Lógicos/Ejercicio 36/ejercicio_36.js
@@ -0,0 +1,8 @@
+const numero1 = 10;
+const numero2 = 20;
+const esIgual = numero1 == numero2;
+const esDiferente = numero1 != numero2;
+
+console.log(`La variable numero1: ${numero1} y numero2: ${numero2} tienen el mismo valor: ${esIgual}`);
+
+console.log(`Las variables numero1: ${numero1} y numero2: ${numero2} son distintas: ${esDiferente}`);

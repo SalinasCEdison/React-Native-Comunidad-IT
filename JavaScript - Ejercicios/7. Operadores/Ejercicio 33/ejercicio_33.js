787dd14b9e716ce0f31aa06038f07cadc0a6bfdd
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-16 14:59:01 -0500

    add: Ejercicio 33 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 33/ejercicio_33.js
@@ -0,0 +1,12 @@
+let cantidadDeUsuario = 100;
+console.log("Se incrementó 5 usuarios");
+console.log(`Cantidad de usuarios: ${cantidadDeUsuario += 5}`);
+
+console.log("Se dieron de baja 3 usuarios");
+console.log(`Cantidad de usuarios: ${cantidadDeUsuario -= 3}`);
+
+console.log("Se duplicaron usuarios");
+console.log(`Cantidad de usuarios: ${cantidadDeUsuario *= 2}`);
+
+console.log("Cuantos hombre y mujeres hay");
+console.log(`Cantidad de usuarios: ${cantidadDeUsuario /= 2}`);

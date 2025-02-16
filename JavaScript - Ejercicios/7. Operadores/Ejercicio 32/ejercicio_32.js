4e9c0b1281a3667c5f2d5e55b68ce0b8745b4810
Salinas Cruz Edison <salinasedisoncruz@gmail.com>
2025-02-16 14:46:07 -0500

    add: Ejercicio 32 - Operadores - JS

---
+++ JavaScript - Ejercicios/7. Operadores/Ejercicio 32/ejercicio_32.js
@@ -0,0 +1,7 @@
+const altura = 10;
+const base = 4;
+
+console.log(`-------- Rectángulo --------`);
+console.log(`La altura es: ${altura}. La base es: ${base}`);
+console.log(`El perímetro es: ${(2 * altura) + (2 * base)}`);
+console.log(`El área es: ${base * altura}`);
